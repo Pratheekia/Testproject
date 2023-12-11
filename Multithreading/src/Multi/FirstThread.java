0e970c4c00f7a24aaed0db05d98abc9e86582085
Pratheek <Pratheek@LAPTOP-25LJ1VDB>
2023-12-11 16:03:40 +0530

    Commit

---
+++ Multithreading/src/Multi/FirstThread.java
@@ -0,0 +1,21 @@
+package Multi;
+
+public class FirstThread {
+
+	public static void main(String[] args) {
+		Thread t=thread current Thread();
+		System.out.println();
+		program p=new program();
+		p.set Name("program thread")
+		p.start()
+	}
+
+}
+
+import java.util.Scanner;
+
+public class pat2 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		 System.out.println("Enter the num" );
---
+++ Multithreading/src/Multi/Program.java
@@ -0,0 +1,11 @@
+package Multi;
+
+public class Program extends Thread {
+
+	public static void run() {
+		Thread t=thr
+		
+
+	}
+
+}
