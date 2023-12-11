d28fd1e75ffff57ffa4ab2f2787c07954bbbc50e
Pratheek <Pratheek@LAPTOP-25LJ1VDB>
2023-12-11 16:07:55 +0530

    Commit

---
+++ programs/src/programs/Sumofages.java
@@ -0,0 +1,21 @@
+package programs;
+
+public class Sumofages {
+
+	public static void main(String[] args) {
+		/*int abhishek=4;
+		int raghav=44;
+		int ananya=36;
+		int prajwal=64;
+		int sum;
+		sum=abhishek+raghav+ananya+prajwal;
+		System.out.println( "the sum of ages of abhishek,raghav,ananya,prajwal  is " + sum);*/
+		byte b=44;
+		short s=36;
+		int i=64;	
+		long l = 9947;
+		System.out.println("the byte ,short, int , long value is "+b + " " +s+ " "+ i +" "+l + " respectively");
+	
+	}
+
+}
